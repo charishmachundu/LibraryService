17e1277e0afb79527b35e3c3752dd65188d0c8ea
charishmachundu <charishmachundu@gmail.com>
2024-02-20 14:35:50 -0600

    Files Added

--- src/main/java/com/cis/batch33/library/LibraryServiceApplication.java
+++ src/main/java/com/cis/batch33/library/LibraryServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LibraryServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(LibraryServiceApplication.class, args);
 	}
 
